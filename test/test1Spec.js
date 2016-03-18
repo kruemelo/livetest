079b477aead153b4dcf918787b03073cc426008a
kruemelo <horatiospapa@web.de>
2016-03-18 18:35:08 +0100

    fixed: tests running twice

--- test/test1Spec.js
+++ test/test1Spec.js
@@ -1,109 +1,113 @@
+;(function () {
+  
+  const path = require('path');
+  const assert = require('chai').assert;
 
-const path = require('path');
-const assert = require('chai').assert;
+  function prepareEnv (testWindow, testPath, callback) {
 
-function prepareEnv (testWindow, testPath, callback) {
-
-  function awaitLoaded (loadingDone) {
-    setTimeout(function () {
-      if (testWindow && testWindow.document.getElementById('btn-clickme')) {
-        return loadingDone();
-      }
-      awaitLoaded(loadingDone);
-    }, 500);
-  }
-
-  testWindow.location.assign(`file://${testPath}/test1.html`);
-  awaitLoaded(callback);
-}
-
-
-describe('test 1 suite', function () {
-
-  var w,
-    testPath;
-
-  const thisSuite = this;
-
-  before(function (done) {
-
-    this.timeout(3000);
-
-    w = thisSuite.ctx.getTestWindow();
-
-    if (!w) {
-      throw new Error('now test window available');
+    function awaitLoaded (loadingDone) {
+      setTimeout(function () {
+        if (testWindow.document.getElementById('btn-clickme')) {
+          return loadingDone();
+        }
+        awaitLoaded(loadingDone);
+      }, 500);
     }
 
-    testPath = path.dirname(thisSuite.file);
+    testWindow.location.assign(`file://${testPath}/test1.html`);
+    awaitLoaded(callback);
+  }
 
-    prepareEnv(w, testPath, done);
-  });
 
+  describe('test 1 suite', function () {
 
-  it('should self-test suite', function () {
-    assert.isObject(thisSuite, 'should have suite');
-    assert.isArray(thisSuite.tests, 'should have tests');
-    assert.isFunction(thisSuite.ctx.getWindow, 'should have suite getWindow()');
-    assert.isFunction(thisSuite.ctx.getTestWindow, 'should have suite getTestWindow()');
-  });
+    var w,
+      testPath;
 
-  it('should self-test test', function () {
-    assert.isObject(this, 'should have this');
-    assert.isObject(this.test, 'should have test');
-  });
+    const thisSuite = this;
 
-  it('should have window context', function (done) {
-    assert.isFunction(this.test.ctx.getWindow, 'should have ctx.getWindow');
-    assert(this.test.ctx.getWindow(), 'should execute ctx.getWindow');
-    assert.isFunction(this.test.ctx.getTestWindow, 'should have ctx.getTestWindow');
-    assert(this.test.ctx.getTestWindow(), 'should execute ctx.getTestWindow');
-    done();
-  });
-  
-  it('should pass', function (done) {
-    setTimeout(function () {
-        done();
-    }, 100);
-  });
+    before(function (done) {
 
-  // it('should fail', function (done) {
-  // 	assert.equal('a', 'b');
-  //   done();
-  // });
-  
-  it.skip('should skip', function (done) {
-    done();
-  });
+      this.timeout(3000);
 
-  describe('inner', function () {
+      w = thisSuite.ctx.getTestWindow();
 
-    it('should pass', function (done) {
-      setTimeout(function () {
-        done();
-      }, 50);
+      if (!w) {
+        throw new Error('now test window available');
+      }
+
+      // testPath = path.dirname(thisSuite.file);
+      testPath = thisSuite.ctx.dirname;
+
+      prepareEnv(w, testPath, done);
     });
 
-    // it('should fail', function (done) {
-    //   done(new Error('Something bad happened!'));
-    // });
 
+    it('should self-test suite', function () {
+      assert.isObject(thisSuite, 'should have suite');
+      assert.isArray(thisSuite.tests, 'should have tests');
+      assert.isFunction(thisSuite.ctx.getWindow, 'should have suite getWindow()');
+      assert.isFunction(thisSuite.ctx.getTestWindow, 'should have suite getTestWindow()');
+    });
+
+    it('should self-test test', function () {
+      assert.isObject(this, 'should have this');
+      assert.isObject(this.test, 'should have test');
+    });
+
+    it('should have window context', function (done) {
+      assert.isFunction(this.test.ctx.getWindow, 'should have ctx.getWindow');
+      assert(this.test.ctx.getWindow(), 'should execute ctx.getWindow');
+      assert.isFunction(this.test.ctx.getTestWindow, 'should have ctx.getTestWindow');
+      assert(this.test.ctx.getTestWindow(), 'should execute ctx.getTestWindow');
+      done();
+    });
+    
+    it('should pass', function (done) {
+      setTimeout(function () {
+          done();
+      }, 100);
+    });
+
+    it('should fail', function (done) {
+    	assert.equal('a', 'b');
+      done();
+    });
+    
     it.skip('should skip', function (done) {
       done();
     });
 
-    it('should access test window dom els', function (done) {
+    describe('inner', function () {
 
-      var btn = w.document.getElementById('btn-clickme'),
-        divResult = w.document.getElementById('result');
+      it('should pass', function (done) {
+        setTimeout(function () {
+          done();
+        }, 50);
+      });
 
-      btn.click();
+      it('should fail', function (done) {
+        done(new Error('Something bad happened!'));
+      });
 
-      assert.strictEqual(divResult.innerHTML, 'yay!');
+      it.skip('should skip', function (done) {
+        done();
+      });
 
-      done();
+      xit('should access test window dom els', function (done) {
+
+        var btn = w.document.getElementById('btn-clickme'),
+          divResult = w.document.getElementById('result');
+
+        btn.click();
+
+        assert.strictEqual(divResult.innerHTML, 'yay!');
+
+        done();
+      });
+
     });
 
   });
 
-});
+}());
--- test/test2Spec.js
+++ test/test2Spec.js
@@ -1,66 +1,70 @@
+;(function () {
+ 
+  const path = require('path');
+  const assert = require('chai').assert;
 
-const path = require('path');
-const assert = require('chai').assert;
+  function prepareEnv (testWindow, testPath, callback) {
 
-function prepareEnv (testWindow, testPath, callback) {
-
-  function awaitLoaded (loadingDone) {
-    setTimeout(function () {
-      if (testWindow.document.getElementById('text')) {
-        return loadingDone();
-      }
-      awaitLoaded(loadingDone);
-    }, 500);
-  }
-
-  testWindow.location.assign(`file://${testPath}/test2.html`);
-  awaitLoaded(callback);
-}
-
-
-describe('test 2 suite', function () {
-
-  var w,
-    mainWindow,
-    testPath;
-
-  const thisSuite = this;
-
-  before(function (done) {
-
-    this.timeout(2000);
-
-    mainWindow = thisSuite.ctx.getWindow();
-    w = thisSuite.ctx.getTestWindow();
-
-    if (!w) {
-      throw new Error('now test window available');
+    function awaitLoaded (loadingDone) {
+      setTimeout(function () {
+        if (testWindow.document.getElementById('text')) {
+          return loadingDone();
+        }
+        awaitLoaded(loadingDone);
+      }, 500);
     }
 
-    testPath = path.dirname(thisSuite.file);
+    testWindow.location.assign(`file://${testPath}/test2.html`);
+    awaitLoaded(callback);
+  }
 
-    prepareEnv(w, testPath, done);
+
+  describe('test 2 suite', function () {
+
+    var w,
+      mainWindow,
+      testPath;
+
+    const thisSuite = this;
+
+    before(function (done) {
+
+      this.timeout(2000);
+
+      mainWindow = thisSuite.ctx.getWindow();
+      w = thisSuite.ctx.getTestWindow();
+
+      if (!w) {
+        throw new Error('now test window available');
+      }
+
+      // testPath = path.dirname(thisSuite.file);
+      testPath = thisSuite.ctx.dirname;
+
+      prepareEnv(w, testPath, done);
+    });
+
+
+    it('should input text', function (done) {
+        var text = w.document.getElementById('text'),
+          divResult = w.document.getElementById('result');
+
+        mainWindow.console.log(text, divResult);
+
+        text.addEventListener('click', function () {
+          mainWindow.console.log('click', this);
+          divResult.innerHTML = this.value;
+        });
+
+        text.value = 'test text';
+        text.click();
+
+        setTimeout(function () {
+          assert.strictEqual(divResult.innerHTML, 'test text');
+          done();
+        }, 200);
+    });
+
   });
-
-
-  it('should input text', function (done) {
-      var text = w.document.getElementById('text'),
-        divResult = w.document.getElementById('result');
-
-      mainWindow.console.log(text, divResult);
-
-      text.addEventListener('click', function () {
-        mainWindow.console.log('click', this);
-        divResult.innerHTML = this.value;
-      });
-
-      text.value = 'test text';
-      text.click();
-
-      setTimeout(function () {
-        assert.strictEqual(divResult.innerHTML, 'test text');
-        done();
-      }, 200);
-  });
-
-});
+  
+}());
